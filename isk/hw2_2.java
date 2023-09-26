5d1510380b47e70780aef45c7b9f2ff582e08d45
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2023-09-26 21:11:03 +0300

    Create hw2_2.java

---
+++ isk/hw2_2.java
@@ -0,0 +1,35 @@
+Задание 2
+ public static void main(String[] args) {
+        try {
+            int d = 0;
+            int[] intArray = {1, 5, 6, 8, 7, 9, 2, 1, 9};
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+
+Задание 3
+
+public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
